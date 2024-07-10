97e6fcdbb930991df66821271b993989fe298e96
Gabrielle Oliveira <oliveiragabrielle662@gmail.com>
2024-07-10 19:30:02 -0300

    feat: update-trip + get-participant + get-trip-details routes

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,9 @@ import { createLink } from './routes/create-link'
 import { getLinks } from './routes/get-links'
 import { getParticipants } from './routes/get-participants'
 import { createInvite } from './routes/create-invite'
+import { updateTrip } from './routes/update-trip'
+import { getTripDetails } from './routes/get-trip-details'
+import { getParticipant } from './routes/get-participant'
 
 const app = fastify()
 
@@ -30,6 +33,9 @@ app.register(createLink)
 app.register(getLinks)
 app.register(getParticipants)
 app.register(createInvite)
+app.register(updateTrip)
+app.register(getTripDetails)
+app.register(getParticipant)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('Server is running on port 3333!')
